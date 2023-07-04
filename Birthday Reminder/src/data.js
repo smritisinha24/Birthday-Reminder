fe02902f527c86ef1586f4fbade53132174455a6
smritisinha24 <116907906+smritisinha24@users.noreply.github.com>
2023-07-04 22:39:55 +0530

    Add files via upload

---
+++ Birthday Reminder/src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'Vanshika Goyal',
+    age: 21,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  // {
+  //   id: 2,
+  //   name: 'Shreya Singh',
+  //   age: 24,
+  //   image:
+  //     'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80',
+  // },
+  {
+    id: 3,
+    name: 'Aryan Kumar',
+    age: 21,
+    image:
+      'https://images.unsplash.com/photo-1519456264917-42d0aa2e0625?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJveSUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60',
+  },
+  // {
+  //   id: 4,
+  //   name: 'Samar Sharma',
+  //   age: 20,
+  //   image:
+  //     'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlJTIwcHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+  // },
+  {
+    id: 5,
+    name: 'Lavanya Sharma',
+    age: 22,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+  {
+    id: 6,
+    name: 'Abhinav Kumar',
+    age: 22,
+    image:
+      'https://microsites.databricks.com/sites/default/files/media/images/dataaisummit_speaker/Prateek.Chawla_1673570758321001iUry.png',
+  },
+];
